4f1663f59644c254d47286a0c28d47e26ddd19d0
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-09-20 08:55:09 -0700

    Get ridda that unnecessary path argument

--- script/upload.sh
+++ script/upload.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-usage="Usage: $(basename "$0") path -- Uploads to the www.jezzamon.com/somepath"
-
-if [ "$#" -ne 1 ]; then
-    echo "$usage"
-    exit 1
-fi
-
 gsutil -m cp index.html gs://www.jezzamon.com/
 gsutil -m cp preview.png gs://www.jezzamon.com/
 gsutil -m cp -r build/ gs://www.jezzamon.com/
